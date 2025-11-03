a77adcb1436be7da639931d041a3b9916435ec00
Nico-hi <nicolasgianfranco123@gmail.com>
2025-11-03 12:57:00 +0100

    log de DB

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import {createPool} from 'mysql2/promise';
 import {DB_HOST,DB_PORT,DB_USER,DB_PASSWD,DB_NAME} from './config.js'
 
+console.log(DB_HOST,DB_PORT,DB_USER,DB_PASSWD,DB_NAME)
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
